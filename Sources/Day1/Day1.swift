267c46644de1031723dc0ed2fcca7cc84f54cf7c
Sean McMains <sean@mcmains.net>
2022-12-02 08:09:54 -0600

    Day 1

--- Sources/Day1/Day1.swift
+++ Sources/Day1/Day1.swift
@@ -1,8 +1,31 @@
 import Algorithms
 
 public func partOne() {
+    let elfCalories = input.caloriesPerElf()
+    print( "Part 1: \(elfCalories.max()!)" )
 }
 
 public func partTwo() {
+    let sortedElfCalories = input.caloriesPerElf().sorted()
+    let c = sortedElfCalories.count
+    let topThreeElfCalories = sortedElfCalories[c-3..<c].reduce(0,+)
+    print( "Part 2: \(topThreeElfCalories)")
+}
 
+extension String {
+    func caloriesPerElf() -> [Int] {
+        var returnValues: [Int] = []
+        var currentElfCalories = 0
+
+        for line in self.split(omittingEmptySubsequences: false, whereSeparator: \.isNewline) {
+            if let currentLineCalories = Int(line) {
+                currentElfCalories += currentLineCalories
+            } else {
+                returnValues.append(currentElfCalories)
+                currentElfCalories = 0
+            }
+        }
+
+        return returnValues
+    }
 }
